c4b9ff38f11cffbcfe195cc34a35fbd57d8f7c3c
Aulia Rachmat Diapari <aulia.diapari@gmail.com>
2023-06-10 11:30:00 +0700

    Completing C2

--- 2. Pytest/test_math.py
+++ 2. Pytest/test_math.py
@@ -3,6 +3,8 @@ This module contains basic unit tests for math operations.
 Their purpose is to show how to use the pytest framework by example.
 """
 
+import pytest
+
 # --------------------------------------------------------------------------------
 # A most basic test function
 # --------------------------------------------------------------------------------
@@ -10,5 +12,14 @@ Their purpose is to show how to use the pytest framework by example.
 def test_one_plus_one():
   assert 1 + 1 == 2
 
+# --------------------------------------------------------------------------------
+# A test function to show assertion introspection
+# --------------------------------------------------------------------------------
+
+def test_one_plus_two():
+  a = 1
+  b = 2
+  c = 3
+  assert a + b == c
 
         
