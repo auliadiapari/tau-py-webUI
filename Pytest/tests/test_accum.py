930a12cd93d4514baaf8f6d28d0821a5c0807de3
auliadiapari <aulia.diapari@gmail.com>
2023-06-12 18:32:39 +0700

    Refresh Commit

--- Pytest/tests/test_accum.py
+++ Pytest/tests/test_accum.py
@@ -44,7 +44,7 @@ def test_accumulator_add_twice(accum):
   accum.add()
   assert accum.count == 2
 
-@pytest.mark.accumulator
-def test_accumulator_cannot_set_count_directly(accum):
-  with pytest.raises(AttributeError, match=r"can't set attribute") as e:
-    accum.count = 10
+# @pytest.mark.accumulator
+# def test_accumulator_cannot_set_count_directly(accum):
+#   with pytest.raises(AttributeError, match=r"can't set attribute") as e:
+#     accum.count = 10
